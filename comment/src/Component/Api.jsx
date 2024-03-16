d79ae07348dbb99fa4f8a57e65e6041ff1bf62a3
yashika277 <yashika.bhadkoliya2707@gmail.com>
2024-03-16 19:27:15 +0530

    conment

--- comment/src/Component/Api.jsx
+++ comment/src/Component/Api.jsx
@@ -157,7 +157,7 @@ const Api = () => {
                     <div class="modal-dialog" role="document">
                       <div class="modal-content">
                         <div class="modal-header">
-                          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                          <h5 class="modal-title" id="exampleModalLabel">Update product</h5>
                           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                           </button>
